8f8816e8bd0dff74f32c2fa637dbc43e322a1a7c
gattygamaun <gattygamaun@gmail.com>
2019-03-12 13:46:23 +0300

    Updates

---
+++ precache-manifest.bfcdd07e4fccc679d39b0f2d5eb2cbc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b43dbb91f06547c0f0d2",
+    "url": "/static/css/main.18e0e736.chunk.css"
+  },
+  {
+    "revision": "b43dbb91f06547c0f0d2",
+    "url": "/static/js/main.b43dbb91.chunk.js"
+  },
+  {
+    "revision": "925d4cd5a93a39689d10",
+    "url": "/static/js/1.925d4cd5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5dd6b48d09a772cfd8152616718fbe20",
+    "url": "/index.html"
+  }
+];
